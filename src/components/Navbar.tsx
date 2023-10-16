59159a1b81ccd7c9dc235f78c0c22c46f19fb1af
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-10-16 22:31:44 +0200

    navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Select</li>
+        <li>Refine</li>
+        <li>Cook</li>
+      </ul>
+    </nav>
+  );
+}
