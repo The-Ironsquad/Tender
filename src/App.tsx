8133cfc1e7b76eecb96b59ce9e7667fa7e623274
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-10-17 20:56:33 +0200

    navbar animation implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
+import { AnimatePresence } from 'framer-motion';
+
 export default function App() {
+  const [showNav, setShowNav] = useState(true);
+
   return (
     <>
-      <h1>Tender</h1>
+      <AnimatePresence>
+        {showNav && <Navbar onClose={setShowNav} />}
+      </AnimatePresence>
+      <h1 onClick={() => setShowNav(true)}>Tender</h1>
       <h2>Your next recipe is just a moment away!</h2>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,35 @@
-export default function Navbar() {
+import { FC } from 'react';
+import styles from './Navbar.module.css';
+
+import { motion } from 'framer-motion';
+
+type PropsType = {
+  onClose: (condition: boolean) => void;
+};
+
+const Navbar: FC<PropsType> = ({ onClose }) => {
+  const closeNavHandler = () => {
+    onClose(false);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Select</li>
-        <li>Refine</li>
-        <li>Cook</li>
-      </ul>
-    </nav>
+    <>
+      <motion.nav
+        className={styles.navbar}
+        initial={{ opacity: 0, x: -150 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -150 }}
+      >
+        <ul className={styles.list}>
+          <li>Home</li>
+          <li>Select</li>
+          <li>Refine</li>
+          <li>Cook</li>
+        </ul>
+      </motion.nav>
+      <div className={styles.modal} onClick={closeNavHandler}></div>
+    </>
   );
-}
+};
+
+export default Navbar;
